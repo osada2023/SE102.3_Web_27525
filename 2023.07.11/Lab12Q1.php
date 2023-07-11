6e980ea04a99bdd4c5cef547a82b8675eba77f4c
osada2023 <135573064+osada2023@users.noreply.github.com>
2023-07-11 13:50:36 +0530

    Add files via upload

---
+++ 2023.07.11/Lab12Q1.php
@@ -0,0 +1,20 @@
+<html>
+<head><title>Arithmatic Operations</title>
+</head>
+<body>
+<?php
+$x=10;
+$y=20;
+$sum = $x + $y;
+
+
+
+echo"sum: ",$x + $y."<br>";
+echo"sum: ",$x - $y."<br>";
+echo"sum: ",$x * $y."<br>";
+echo"sum: ",$x / $y."<br>";
+
+?>
+
+</body>
+</html>
---
+++ 2023.07.11/Lab12Q2.php
@@ -0,0 +1,13 @@
+<html>
+<head><title>Q2</title>
+</head>
+<body>
+<?php
+for($i=5; $i<=15; $i++){
+echo $i." ";
+}
+
+?>
+
+</body>
+</html>
